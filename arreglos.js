d772c44c9189335aaa1c957f341a008eb3ce7e86
Luisner Arrieta <luisner.arrieta82@gmail.com>
2022-09-18 20:37:58 -0400

    agregamos arreglo

---
+++ arreglos.js
@@ -0,0 +1,1 @@
+const frutas = ['cambur','manzana','naranja','pera'];
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const nombre = 'Prueba XYZ'
+let nombre = 'Luisner'
+console.log(`Hola buen dia como estas ${nombre}`)
+nombre = 15
 console.log(`Hola buen dia como estas ${nombre}`)
